2119d4241e2b2ff83a6f4fb1f9f978c90a47cdc2
yarangit-coder <yarangit@gmail.com>
2026-06-06 09:58:08 +0700

    Create 01-3.c

---
+++ capter01/prog/01-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // กำนหดตัวแปร ตัวเลข
+    int age = 20;
+  
+    // กำหนดตัวแปร จำนวนทิศนิยม
+    float height = 5.7;
+  
+    // กำหนดตัวแปร ตัวอักษร
+    char grade = 'A';
+  
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+  
+    return 0;
+}
